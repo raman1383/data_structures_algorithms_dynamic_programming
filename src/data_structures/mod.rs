7920918ff0ba402c89927d162034f0dfeb37fdd8
raman1383 <raman83shariati@gmail.com>
2022-06-23 21:23:00 +0430

    r

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -3,5 +3,6 @@ pub mod hashtable;
 pub mod lists;
 pub mod queues;
 pub mod stack;
+pub mod suffix_array;
 pub mod trees;
 pub mod union;
--- src/data_structures/trees/fenwick.rs
+++ src/data_structures/trees/fenwick.rs
@@ -4,4 +4,5 @@ pub struct FenwickTree {}
 impl FenwickTree {
     pub fn range_query() {}
     pub fn point_updates() {}
+    pub fn construction() {}
 }
