f1b16edccb6fd82d880bcebc2a1623a1d05f2d08
raman1383 <raman83shariati@gmail.com>
2022-06-23 23:15:28 +0430

    e

--- src/data_structures/queues/mod.rs
+++ src/data_structures/queues/mod.rs
@@ -1,3 +1,4 @@
 pub mod binary_heap;
+pub mod indexed_priority_queue;
 pub mod priority_queue;
 pub mod queue;
--- src/data_structures/trees/balanced_binary_search_tree.rs
+++ src/data_structures/trees/balanced_binary_search_tree.rs
@@ -2,6 +2,6 @@ pub struct BalancedBinarySearchTree {}
 
 impl BalancedBinarySearchTree {
     pub fn rotations() {} // check all cases
-    pub fn remove() {}
+    pub fn remove() {} // find then replace
     pub fn insert() {}
 }
