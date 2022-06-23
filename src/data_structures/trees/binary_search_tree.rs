ede567b122e0abe5cc6e40d401897005bf38bfa3
raman1383 <raman83shariati@gmail.com>
2022-06-23 12:47:28 +0430

    r

--- src/data_structures/trees/binary_search_tree.rs
+++ src/data_structures/trees/binary_search_tree.rs
@@ -3,5 +3,11 @@ pub struct BinarySearchTree {}
 impl BinarySearchTree {
     pub fn insert() {}
     pub fn find() {}
-    pub fn remove() {}
+    pub fn remove() {
+        // with no subtree
+
+        // with one subtree
+
+        // with two subtree
+    }
 }
--- src/data_structures/trees/binary_tree.rs
+++ src/data_structures/trees/binary_tree.rs
@@ -3,5 +3,11 @@ pub struct BinaryTree {}
 impl BinaryTree {
     pub fn insert() {}
     pub fn find() {}
-    pub fn remove() {}
+    pub fn remove() {
+        // with no subtree
+
+        // with one subtree
+
+        // with two subtree
+    }
 }
