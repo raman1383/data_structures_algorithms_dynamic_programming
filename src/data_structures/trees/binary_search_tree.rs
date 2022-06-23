30efc789f1008a577131fe02446e8529a3347c5e
raman1383 <raman83shariati@gmail.com>
2022-06-23 12:42:18 +0430

    r

--- src/data_structures/trees/binary_search_tree.rs
+++ src/data_structures/trees/binary_search_tree.rs
@@ -2,6 +2,6 @@ pub struct BinarySearchTree {}
 
 impl BinarySearchTree {
     pub fn insert() {}
+    pub fn find() {}
     pub fn delete() {}
-    pub fn search() {}
 }
--- src/data_structures/trees/mod.rs
+++ src/data_structures/trees/mod.rs
@@ -1,2 +1,3 @@
+pub mod balanced_binary_search_tree;
 pub mod binary_search_tree;
 pub mod binary_tree;
