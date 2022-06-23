bb2747ca4a629c4e631229e1fe2db3efb313bad2
raman1383 <raman83shariati@gmail.com>
2022-06-23 23:34:41 +0430

    r

---
+++ src/data_structures/graph/graph.rs
@@ -0,0 +1,13 @@
+pub struct Graph {}
+// struct Node {}
+// struct Edge {}
+
+impl Graph {
+    pub fn depth_first_traversal() {
+        // using a stack
+    }
+    pub fn breadth_first_traversal() {
+        // using a queue
+    }
+    pub fn shortest_path() {}
+}
