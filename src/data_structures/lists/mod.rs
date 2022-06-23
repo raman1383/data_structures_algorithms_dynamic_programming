bfa2982e29f8358a7082584d206594e3b1e22249
raman1383 <raman83shariati@gmail.com>
2022-06-23 09:47:40 +0430

    r

---
+++ src/data_structures/lists/mod.rs
@@ -0,0 +1,4 @@
+pub mod double_linked_list;
+pub mod dynamic_list;
+pub mod single_linked_list;
+pub mod static_list;
--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -1,4 +1,1 @@
-pub mod double_linked_list;
-pub mod dynamic_list;
-pub mod single_linked_list;
-pub mod static_list;
+pub mod lists;
