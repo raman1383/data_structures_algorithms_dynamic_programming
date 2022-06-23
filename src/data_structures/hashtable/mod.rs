0ad3453f4dd2de43095ae4dc82b0a151164a5b74
raman1383 <raman83shariati@gmail.com>
2022-06-23 21:01:55 +0430

    r

--- src/data_structures/hashtable/mod.rs
+++ src/data_structures/hashtable/mod.rs
@@ -1,2 +1,3 @@
 pub mod hash_map;
 pub mod linked_list_separate_chaining;
+pub mod open_addressing;
---
+++ src/data_structures/hashtable/open_addressing.rs
@@ -0,0 +1,8 @@
+pub struct HashTable {}
+
+impl HashTable {
+    pub fn linear_probing() {}
+    pub fn quadratic_probing() {}
+    pub fn double_hashing() {}
+    pub fn remove() {}
+}
